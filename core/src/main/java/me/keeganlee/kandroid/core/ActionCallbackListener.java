4b51d05c8e2f4e667e4566c7acc2e6eb8b7d769b
Keegan Lee <keegan.lee704@gmail.com>
2015-07-06 14:20:54 +0800

    change package name

---
+++ core/src/main/java/me/keeganlee/kandroid/core/ActionCallbackListener.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2015. Keegan小钢（http://keeganlee.me）
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.keeganlee.kandroid.core;
+
+/**
+ * Action的处理结果回调监听器
+ *
+ * @version 1.0 创建时间：15/6/25
+ */
+public interface ActionCallbackListener<T> {
+    /**
+     * 成功时调用
+     *
+     * @param data 返回的数据
+     */
+    public void onSuccess(T data);
+
+    /**
+     * 失败时调用
+     *
+     * @param errorEvent 错误码
+     * @param message    错误信息
+     */
+    public void onFailure(String errorEvent, String message);
+}
